14d02c80a29002b29ec997528fb99f1c067af639
Cmeredith <tina.wheezy@gmail.com>
2018-10-09 14:50:26 -0400

    making word stats attempt 1

---
+++ main.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+  el: '#app',
+
+  data: {
+    textInput: "",
+    pcount: 0,
+    scount: 0,
+    wcount: 0,
+    ccount: 0,
+    frequentchar: "",
+    longestword: "",
+    w_array: [],
+    c_array: []
+
+  },
+
+  methods: {
+
+      inputMagic: function () {
+
+          for (let i = 0; i < this.textInput.length; i++){
+            if (this.textInput[i] == "!" || this.textInput[i] == "." || this.textInput[i] == "?"){
+                this.scount++;
+            }
+          }
+
+        this.w_array = this.textInput.split(" ");
+        this.wcount = this.w_array.length;
+        this.c_array = this.textInput.split("");
+        this.ccount = this.c_array.length;
+
+
+
+      }
+
+  }
+})
